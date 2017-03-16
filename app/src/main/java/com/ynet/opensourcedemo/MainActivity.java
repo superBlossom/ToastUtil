7709d9f676c2bba1dc47cbcb5ec8ad32b1266bf4
zhaopeng <1216761420@qq.com>
2017-03-16 19:16:16 +0800

    添加library引用实例

--- app/src/main/java/com/ynet/opensourcedemo/MainActivity.java
+++ app/src/main/java/com/ynet/opensourcedemo/MainActivity.java
@@ -3,11 +3,14 @@ package com.ynet.opensourcedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ynet.toastlibrary.ToastUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtils.showToast(this, "ppp");
     }
 }
